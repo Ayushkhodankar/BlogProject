335a52b8ee85d52c1ac1ff6cb7e926ae38481d81
Ayush Khodankar <ayushkhodankar01@gmail.com>
2023-01-08 14:58:11 +0530

    initial commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className='border-b-4 border-green-700 text-center fixed top-0 bg-green-600 font-bold w-full text-lg text-white'>
+    <nav className='border-b-4 border-red-700 text-center fixed top-0 bg-green-600 font-bold w-full text-lg text-white'>
       <ul>
         <li className='inline-block py-4'>
           <Link to='/' className='pl-6 pr-8'>
